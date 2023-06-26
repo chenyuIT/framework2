76613e8cda7366c5885a8a4b18cfb6c475a2150c
chenyuIT <117488608+chenyuIT@users.noreply.github.com>
2023-06-26 14:08:19 -0700

    Update app.go

--- facades/app.go
+++ facades/app.go
@@ -2,7 +2,7 @@ package facades
 
 import (
 	foundationcontract "github.com/chenyuIT/framework/contracts/foundation"
-	"github.com/chenyuIT/framework/foundation"
+	"github.com/chenyuIT/framework2/foundation"
 )
 
 func App() foundationcontract.Application {
